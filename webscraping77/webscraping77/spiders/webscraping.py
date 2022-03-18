b77e70ec6bc4a350b062c720797afe64f1bf726f
Thiago Utsch <thiago.utsch.77@outlook.com>
2022-03-18 19:47:21 -0300

    +outrasloterias

--- webscraping77/webscraping77/spiders/webscraping.py
+++ webscraping77/webscraping77/spiders/webscraping.py
@@ -5,8 +5,22 @@ class WebscrapingSpider(scrapy.Spider):
     name = 'webscraping'
     start_urls = ['https://www.redeloteria.com.br/']
 
+
     def parse(self, response):
-        for mega in response.css('.numberCircleMega'):
+
             yield{
-                'resultado': response.css('.numberCircleMega::text').getall()
+                'Mega-Sena': response.css('h2:nth-child(6)::text').getall(),
+                'Resultado': response.css('.numberCircleMega::text').getall()
+            }
+            yield{
+                'Lotofacil': response.css('h2:nth-child(11)::text').getall(),
+                'Resultado': response.css('.numberCircleLotoFacil::text').getall()
+            }
+            yield{
+                'Quina': response.css('h2:nth-child(16)::text').getall(),
+                'Resultado': response.css('.numberCircleQuina::text').getall()
+            }
+            yield {
+                'Lotomania': response.css('h2:nth-child(21)::text').getall(),
+                'Resultado': response.css('.numberCircleLotoMania::text').getall()
             }
